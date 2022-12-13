0f54c9b1e16274bdf70233927c2f51d4a7452aa5
glemaster <gwynethsage@gmail.com>
2022-12-12 21:00:33 -0800

    adding cmake build system

---
+++ CMakeFiles/CPPTutorial.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPPTutorial.dir/src/main.cpp.o"
+  "CMakeFiles/CPPTutorial.dir/src/main.cpp.o.d"
+  "CPPTutorial"
+  "CPPTutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPPTutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test/build/CMakeFiles/CPPTutorial-test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPPTutorial-test.dir/main.o"
+  "CMakeFiles/CPPTutorial-test.dir/main.o.d"
+  "CPPTutorial-test"
+  "CPPTutorial-test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPPTutorial-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
